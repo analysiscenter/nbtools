7b18ed93622b96b7c95b4b4afef66f4a0d92950c
HollowPrincess <risahawkeye@mail.ru>
2022-03-15 12:32:54 +0300

    add run_notebook into nbtools

--- nbtools/__init__.py
+++ nbtools/__init__.py
@@ -1,5 +1,6 @@
 """ Init file. """
 from .pylint_notebook import pylint_notebook
+from .run_notebook import run_notebook
 from .core import StringWithDisabledRepr, notebook_to_script
 
 __version__ = '0.1.0'
