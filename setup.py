6d6cfb2c3c893d6396ce883a33021455a328a839
alexeykozhevin <alexey.a.kozhevin@gmail.com>
2024-07-05 09:20:27 +0000

    Update dependencies

--- setup.py
+++ setup.py
@@ -27,14 +27,19 @@ setup(
     zip_safe=False,
     platforms='any',
     install_requires=[
-        'nvidia-ml-py3>=7.352',
+        'pynvml>=11.5.0',
         'blessed>=1.17',
         'psutil>=5.6',
     ],
-    extras_require={'nbrun': [
-        'ipython>=7.10.0',
-        'nbconvert>=5.6.1',
-    ]},
+    extras_require={
+        'nbrun': [
+            'ipython>=7.10.0',
+            'nbconvert>=5.6.1',
+        ],
+        'nbwatch': [
+            'requests>=2.30'
+        ]
+    },
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
