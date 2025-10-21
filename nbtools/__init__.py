e1795e58bcd25c356493cef899e83e6d58d66d8a
alexeykozhevin <alexey.a.kozhevin@gmail.com>
2025-10-21 11:18:40 +0000

    Add __version__ attr

--- nbtools/__init__.py
+++ nbtools/__init__.py
@@ -1,4 +1,11 @@
 """ Init file. """
+from importlib.metadata import PackageNotFoundError, version
+
 from .core import *
 from .exec_notebook import exec_notebook, run_notebook
 from .pylint_notebook import pylint_notebook
+
+try:
+    __version__ = version("py-nbtools")
+except PackageNotFoundError:
+    __version__ = "0.0.0"  # e.g., running from a source tree
